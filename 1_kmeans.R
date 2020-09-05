715575c4484ad909cedf9f7b8d0254b37fa8bd3d
TomBearpark <t.bearpark@googlemail.com>
2020-09-05 09:28:16 +0100

    removed un-needed analysis

--- 1_kmeans.R
+++ 1_kmeans.R
@@ -1,15 +1,21 @@
 # Note: to run this if you aren't Tom, change the "dir" string to the location
 # of the input data on your machine, and make sure you have 
 # dplyr and ggplot2 libraries
+# The fuzzy kmeans analysis at the end also requires the "e1071" library
+# I use dplyr and ggplot2 rather than the base R version provided by the professor
+# due to improved plot aesthetics
 
 # Contents: 
 # 1. Just running the code that the prof put on his website.
 # 2. Replicate using dplyr and ggplot, plot graphs asked for by exercise sheet.
+# 2.1 Density plot
+# 2.2 Kmeans analysis
+# 2.3 Unscaled Kmeans analysis
+# 2.4 Extensions: Fuzzy kmeans and sum of squares as function of K
 
 rm(list = ls())
 dir = '/Users/tombearpark/Documents/princeton/1st_year/ECO517/exercises/'
 load(paste0(dir, "data/asciiqob.rdata"))
-
 
 ########################################
 # Version 1 
@@ -27,24 +33,25 @@ akv1 <- akv1 %*% diag(1/sigakv1)
 # Plot
 kout <- kmeans(akv1, 2, iter.max=100)
 plot(akv1[ , 2], akv1[ , 1], col= kout$cluster)
-kout$centers %*% diag(sigakv1)
 
+# recover variable centers (unscaled)
+kout$centers %*% diag(sigakv1)
 
 
 ########################################
 # Version 2 
 
 # Tidyverse version
-
 library(dplyr)
 library(ggplot2)
+
+# 2.1 Density plot
 
 # Rescale by standard deviation for vars of interest
 df = akdataf %>% 
   mutate(logwage_scaled = logwage / sd(akdataf$logwage), 
          educ_scaled = educ / sd(akdataf$educ)) 
   
-
 # Check out a density plot of the situation - just the raw values to start with
 ggplot(data = df) +
   geom_bin2d(aes(x = educ_scaled, y = logwage_scaled), alpha = 0.7) +
@@ -53,6 +60,8 @@ ggplot(data = df) +
   
 ggsave(filename = paste0(dir, "/1_week/raw_data_heat_map.png"))
 
+
+# 2.2 Kmeans analysis
 # Function for adding a column with contains the cluster allocated by kmean 
 get_plot_df = function(data, k){
   
@@ -66,9 +75,6 @@ get_plot_df = function(data, k){
   
   return(data_subset)
 }
-
-# Test function
-# plot_df = get_plot_df(df, 2)
 
 # Get a long dataframe for plotting, with different cluster numbers as id
 plot_df = lapply(
@@ -86,79 +92,7 @@ ggplot(data = plot_df) +
 ggsave(filename = paste0(dir, "/1_week/kmeans_log.png"))
 
 
-
-
-# Plot the centroids used in each k-means calculation run
-get_cluster_info = function(data, k){
-  
-  data_subset = data %>% 
-    select(c(logwage_scaled, educ_scaled)) 
-  
-  info = as.data.frame(kmeans(data_subset, k, iter.max=100)$centers)
-  info$k = k
-
-  return(info)
-}
-
-c_vals = lapply(
-                c(2,3,4,5),
-                get_cluster_info, data = df) %>% 
-                bind_rows()
-
-ggplot(data = c_vals) +
-  geom_point(aes(x = educ_scaled, y = logwage_scaled, 
-                 color = as.factor(k)), size = 5) +
-  theme_bw() +
-  ggtitle("Centroids chosen for each value of K")
-
-ggsave(filename = paste0(dir, "/1_week/kmeans_centroids.png"))
-
-
-# Combined centroids + cluster version
-
-ggplot() +
-  geom_point(data = plot_df, aes(x = educ_scaled, y = logwage_scaled, 
-                 col = cluster), alpha = 0.2) +
-  geom_point(data = c_vals %>% 
-               mutate(num_clusters = as.factor(k)), 
-             aes(x = educ_scaled, y = logwage_scaled, 
-                 color = num_clusters), size = 5, shape = 4, 
-             color = "black") +
-  theme_bw() + 
-  facet_wrap(~num_clusters) 
-
-ggsave(filename = paste0(dir, 
-                         "/1_week/kmeans_combined_scatter_and_centroids.png"))
-
-
-
-# extension, not sure if this is useful?
-# Is there any big jumps in model fit with increases in k?
-get_ss_info = function(data, k){
-  
-  data_subset = data %>% 
-    select(c(logwage_scaled, educ_scaled)) 
-  
-  info = as.data.frame(kmeans(data_subset, k, iter.max=100)$tot.withinss)
-  info$k = k
-  names(info) = c("total_wss", "k")
-  
-  return(info)
-}
-w_vals = lapply(
-  c(1, 2,3,4,5,6,7),
-  get_ss_info, data = df) %>% 
-  bind_rows()
-
-ggplot(data = w_vals) +
-  geom_point(aes(x= k, y = total_wss)) +
-  theme_bw()
-
-ggsave(filename = paste0(dir, "/1_week/kmeans_within_cluster_by_K.png"))
-
-
-
-
+# 2.3 Unscaled Kmeans analysis
 
 # Try a version without using scaling of logwage/education 
 
@@ -173,36 +107,52 @@ plot_df_raw = plot_df_raw %>%
 ggplot(data = plot_df_raw) +
   geom_point(aes(x = educ, y = logwage, col = cluster), alpha = 0.7) +
   theme_bw() 
-
-# Raw density plot looks very qualitatively similar to the scaled version
-# ggplot(data = plot_df_raw) +
-#   geom_bin2d(aes(x = educ, y = logwage), alpha = 0.7) +
-#   scale_fill_continuous(type = "viridis", trans = "log", guide = FALSE) +
-#   theme_bw() 
-
 ggsave(filename = paste0(dir, "/1_week/kmeans_wage_not_logged.png"))
 
 
+# 2.4 Extensions: Fuzzy kmeans and sum of squares as function of K
 
+# 2.4.1 Try fuzzy clustering algorithm
 
-# Try fuzzy clustering algorithm
-
-pacman::p_load('e1071')
+library(e1071)
 data_subset = df %>% 
   select(c(logwage_scaled, educ_scaled)) 
 cm = cmeans(data_subset, 2)
 
-# Create an index of certainty
+# Create an index of certainty. This is somewhat adhoc
 plot_cm = cbind(data_subset, cm$membership) %>% 
   mutate(group = ifelse(`1` > 0.5,1,2)) %>% 
   mutate(Certainty_level = `1`-0.5)
 
 ggplot(data = plot_cm) +
   geom_point(aes(x = educ_scaled, y = logwage_scaled, 
-                  color = Certainty_level)) +
+                 color = Certainty_level)) +
   theme_bw()+
   scale_colour_gradient2()
 ggsave(filename = paste0(dir, "/1_week/fuzzy_kmeans.png"))
+
+# 2.4.2. Is there any big jumps in model fit with increases in k?
+get_ss_info = function(data, k){
+  
+  data_subset = data %>% 
+    select(c(logwage_scaled, educ_scaled)) 
+  
+  info = as.data.frame(kmeans(data_subset, k, iter.max=100)$tot.withinss)
+  info$k = k
+  names(info) = c("total_wss", "k")
+  
+  return(info)
+}
+w_vals = lapply(
+  c(1,2,3,4,5,6,7),
+  get_ss_info, data = df) %>% 
+  bind_rows()
+
+ggplot(data = w_vals) +
+  geom_point(aes(x= k, y = total_wss)) +
+  theme_bw()
+
+ggsave(filename = paste0(dir, "/1_week/kmeans_within_cluster_by_K.png"))
 
 
 
