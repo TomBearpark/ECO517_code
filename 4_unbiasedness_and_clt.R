9f2aa597f18a6bb9f7b02ab52077078bdd6c33c4
TomBearpark <t.bearpark@googlemail.com>
2020-09-23 12:38:34 +0100

    commented sims code

--- 4_unbiasedness_and_clt.R
+++ 4_unbiasedness_and_clt.R
@@ -5,8 +5,10 @@ library(ggplot2) # plots
 library(data.table) # fread command to easily read in this nasty asc file
 library(testthat) # Assert statements
 library(patchwork) # Combining ggplot objects
-set.seed(123)
-theme_set(theme_bw())
+set.seed(123) # Make random numbers replicable
+theme_set(theme_bw()) # ggplot theme
+
+# Output location string
 dir = "/Users/tombearpark/Documents/princeton/1st_year/ECO517/exercises/4_week/"
 
 ##################################
@@ -45,7 +47,15 @@ df_30_s = lapply(seq(1,30, 1), get_sample, values = values) %>%
 ggplot(data =df) +
     geom_histogram(aes(x = population), alpha = 0.5, fill = "blue") + 
     ggtitle("Histogram of full sample population values")
-ggsave(file= paste0(dir, "/full_sample_raw_histogram.png"))
+
+ggsave(file= paste0(dir, "/full_sample_raw_histogram.png"), height = 5, width = 5)
+
+ggplot(data = df %>% filter(population < 10000)) +
+    geom_histogram(aes(x = population), alpha = 0.5, fill = "blue") + 
+    ggtitle("Histogram of sample population values, pop < 10000")
+ggsave(file= paste0(dir, "/small_pop_cities_sample_histogram.png"), height = 5, width = 5)
+
+
 
 # 1. Display histogram of sample means
 df_means = df_30_s %>% 
@@ -58,9 +68,9 @@ ggplot(data =df_means) +
     labs(caption = 
     "Means are from 30 randomly drawn samples, with replacement. Each sample has size 20.")
 
-ggsave(file= paste0(dir, "/means_of_30_samples_histogram.png"))
+ggsave(file= paste0(dir, "/means_of_30_samples_histogram.png"), height = 5, width = 5)
 
-# 2. Diplay a qqplot
+# 2. Diplay a qqplot - using SIMS code rather than ggplot2 :(
 df_means$len = 1:30
 png(file=paste0(dir, "/qqplot_sample_means.png"))
     qqnorm(df_means$Sample_mean, col = "steelblue")
@@ -68,7 +78,7 @@ dev.off()
 
 
 # Extention - can we make it seem more like a normal if we take more s
-# samples, of bigger size?
+# samples, of bigger size? Answer: yes!
 
 # Lets try taking 1000 samples, each of size 200
 plot_means = function(n_samples, size_sample){
@@ -84,21 +94,27 @@ plot_means = function(n_samples, size_sample){
     p = ggplot(data =df_means_big_s) +
             geom_histogram(aes(x = Sample_mean), alpha = 0.5, fill = "blue") + 
             ggtitle(paste0(
-                "No Samples ", n_samples))
+                "Samples=", n_samples, ", Size=", size_sample)) 
     return(p)
 }
-# note - this is a big sample - takes a while to run! 
+# note - this is a big sample - takes a while to run (like 5 mins on my laptop)! 
 p = plot_means(n_samples = 100,size_sample = 30) + 
         plot_means(n_samples = 1000,size_sample = 30) + 
         plot_means(n_samples = 10000,size_sample = 30) 
 
+ggsave(p, file= paste0(dir, "/means_of_many_size_30_samples_histogram.png"))
+
 q = plot_means(n_samples = 100,size_sample = 300) + 
         plot_means(n_samples = 1000,size_sample = 300) + 
         plot_means(n_samples = 10000,size_sample = 300) 
+ggsave(q, file= paste0(dir, "/means_of_many_size_300_samples_histogram.png"))
 
 r = plot_means(n_samples = 1000,size_sample = 3000) + 
         plot_means(n_samples = 10000,size_sample = 3000) + 
         plot_means(n_samples = 100000,size_sample = 3000) 
+ggsave(r, file= paste0(dir, "/means_of_many_size_3000_samples_histogram.png"))
+
+ggsave(p/q/r, file= paste0(dir, "/means_of_many_samples_histogram.png"))
 
 
 ##################################
@@ -106,30 +122,47 @@ r = plot_means(n_samples = 1000,size_sample = 3000) +
 
 # Consider model where iid data drawn from normal N(mu, mu^2)
 
-# Set 
+# Set sample size and number of draws
 N <- 20
 ndraw <- 30
 
-lmpdf <- function(mu,xbar, s,N) -N * log(abs(mu)) - .5 * N *(s^2 + (xbar- mu)^2)/mu^2
-## This is the kernel of the log posterior pdf for a sample of size N, sample
-## mean xbar, and sample standard deviation s.
-N <- 20
-ndraw <- 30
+## This function returns the kernel of the log posterior pdf for a sample of size N, 
+## sample mean xbar, and sample standard deviation s.
+lmpdf <- function(mu,xbar, s,N) {
+    -N * log(abs(mu)) - .5 * N *(s^2 + (xbar- mu)^2)/mu^2
+}
+
+# Get matrix of draws
 mms <- matrix(rnorm(N * ndraw, mean=1, sd=1), N, ndraw)
+
+# Find means for each sample
 mmean <- apply(mms, 2, mean)
+
+#  find s^2 for each sample
 mmsigsq <- apply(mms, 2, var) * (N-1)/N   #R uses N-1 in denominator
+
+# Initialise vector - zeros of length ndraw
 mnorm <- vector("numeric", ndraw)
 mhat <- mnorm
-mmax <- max(lmpdf(seq(-.5, 1.5, length=300), mmean[1], sqrt(msigsq[1]), N))
+
 ## we need a rough value of mmax to scale the log likelihood when exponentiating.
-for (id in 1:ndraw) mnorm[id] <- integrate(function(mu)exp(lmpdf(mu, mmean[id], sqrt(msigsq[id]), N)-mmax), lower=.5, upper=1.5)$value
-## above computes the scale factor of the likelihood in each sample that makes it
+mmax <- max(lmpdf(seq(-.5, 1.5, length=300), mmean[1], sqrt(msigsq[1]), N))
+
+## Compute the scale factor of the likelihood in each sample that makes it
 ## integrate to one.
-##
 ## This range of integration works for N=20. Might need adjustment for other N's.
-##
-for (id in 1:30) mhat[id] <- integrate(function(mu) mu * exp(lmpdf(mu, mmean[id], sqrt(msigsq[id]),N)-mmax), lower=.5, upper=1.5)$value/mnorm[id]
+for (id in 1:ndraw){
+    mnorm[id] <- integrate(function(mu)exp(lmpdf(mu, mmean[id], 
+        sqrt(msigsq[id]), N)-mmax), lower=.5, upper=1.5)$value
+} 
+
+# Find the posterior means
+for (id in 1:30) {
+    mhat[id] <- integrate(function(mu) mu * exp(lmpdf(mu, mmean[id], 
+       sqrt(msigsq[id]),N)-mmax), lower=.5, upper=1.5)$value/mnorm[id]
+}
+
 ## mhat should be the posterior means.
 ## mmean should be the unbiased sample means
-##
+
 
