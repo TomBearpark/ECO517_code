310f2131558a4ed88aad799cfad4e77463bca192
TomBearpark <t.bearpark@googlemail.com>
2020-09-28 18:10:03 +0100

    added AK replication extension

--- 5_returns_to_schooling.R
+++ 5_returns_to_schooling.R
@@ -14,7 +14,7 @@
 rm(list = ls())
 library(dplyr) # data manipulation, and piping
 library(ggplot2) # plots
-library(tidyr) # pivot data from long to wide
+# library(tidyr) # pivot data from long to wide
 
 # library(data.table) # fread command to easily read in this nasty asc file
 # library(testthat) # Assert statements
@@ -30,14 +30,6 @@ load(url("http://sims.princeton.edu/yftp/emet1_2020/kmeans/akdata.RData"))
 df = akdataf 
 
 
-
-# Check the data out...
-# ggplot(data = df, aes(x = educ, y = logwage)) +
-#     geom_point() + 
-#     geom_smooth(method = "lm")
-
-
-
 ##################################
 # Method 1
 # Assume that we know the se of means is the sigma
@@ -48,7 +40,7 @@ df_a = df %>%
     summarise(mu_s = mean(logwage), 
                 n = n(), 
                 sd = sd(logwage), 
-                se = sd / sqrt(n))
+                se = sd(logwage) / sqrt(n))
 # Plot output
 ggplot(data = df_a) +
     geom_point(aes(x = educ, y = mu_s), color = "red") +
@@ -73,67 +65,49 @@ draw_and_test = function(i, mu, sigma) {
     draw = rnorm(length(mu), 
                  mean = mu, sd = sigma)[-1]
     
-    # Check 1: monotone
-    check1 = 1*all(diff(draw)>0)
-    
-    # Check 2: 
-    check2 = 1*(draw[12] - draw[11] >  draw[11] - draw[10] )
-    
-    # Check 3: monotone 5 to 16
-    check3 = 1*all(diff(draw[seq(5,16)])>0)
-        
-    # Check 4: 
-    
-    
-    # Check 5: 
-    
-    
-    df = data.frame(draw = rnorm(length(mu), 
-                    mean = mu, sd = sigma), 
-                    draw_num = i) %>% 
-        mutate(educ = row_number())
+    # Do tests and collect results
+    df = data.frame(
+        # Draw indicator
+        draw = i, 
+        # Check 1: monotone
+        check1 = 1*all(diff(draw)>0), 
+        # Check 2: 
+        check2 = 1*(draw[12] - draw[11] >  draw[11] - draw[10]),
+        # Check 3: monotone 5 to 16
+        check3 = 1*all(diff(draw[seq(5,16)])>0), 
+        # Check 4: monotone 17 to 20
+        check4 = 1*all(diff(draw[seq(17,20)])>0), 
+        # Check 5: 
+        check5 = 1*(draw[16] - draw[12] >  draw[12] - draw[8])
+    )
     
     return(df)
 }
 
-# Take draws - reshape for easier manipulation
-draws = lapply(1:2, draw, mu = mu, sigma = sigma) 
-diff(c(1,2))
+# Take draws
+num_draws = 1000
 
+df_a = lapply(seq(1, num_draws), 
+    draw_and_test, mu = mu, sigma = sigma) %>% 
+    bind_rows()
 
-
-check1 = apply(
-    diff(draws[[1:length(draws)]]$draw) < 0,
-    1,
-    all)
-
-
-%>% 
-    bind_rows() %>% 
-    pivot_wider(id_cols = draw_num, 
-                names_from = educ, names_prefix = "D_", 
-                values_from = draw)
-
-# Function 
-all(diff(draws[[1]]$draw) <0)
-
-check1=apply(diff(draws[,1:ncol(draws)])<0,2,all)
-
-
-draws["D_1"] < draws["D_2"] < draws["D_3"] < draws["D_4"]
-
-check_monotonicity_list = function(start_draw, end_draw, df){
-    length(draws$draw_num)
-    start_draw = 1
-    end_draw = 3
-    for (D in seq(start_draw, end_draw)){
-        D = 1
-        d2 = D+ 1
-        as.numeric(df[,paste0("D_",D)]) - as.numeric(df[paste0("D_", D + 1)])
-    }
+for (i in 1:5){
+    print(paste0("Num draws satisfying test ", i, ": ",
+                 sum(df_a[paste0("check", i)])))
 }
 
 
+##################################
+# Method 2
+
+# Use the full Normal-inverse-gamma: For each draw of the vector µ, first draw 
+# a vector of σS values from their respective inverse-gamma distributions, 
+# then use those σS values to generate draws from the corresponding conditional 
+# normal joint distributions for µ.
+
+
+# draw vector of sigma_s
+rgamma()
 
 
 
@@ -143,43 +117,38 @@ check_monotonicity_list = function(start_draw, end_draw, df){
 
 
 
+# https://www.jstor.org/stable/pdf/2937954.pdf?refreqid=excelsior%3A875da335947556d4de1c669ef34fbba5
+
+# 1
+# replication of AK results
+df_ak = df %>% mutate(y_and_q = yob + 0.25 * qob) %>% 
+    group_by(y_and_q) %>% summarise(years_completed = mean(educ))
+# fig1
+ggplot(data = df_ak) +
+    geom_line(aes(x = y_and_q,y=years_completed))
+# Table five, column 1
+lm(logwage ~ educ + as.factor(yob), data = df)
+
+# 2 Look for some non-linearity... 
+LMs = c()
+col = c()
+for (i in seq(0,18,3)){
+    print(i)
+    j = i+3
+    LMs = c(LMs,
+        lm(logwage~educ +  as.factor(yob), 
+            data = df %>% 
+               filter(educ >= !!i,
+                   educ < !!j))$coefficients["educ"])
+    col = c(col, paste0(i, " to ", j, " years educ"))
+    
+}
+replic_df = data.frame(coefficient = LMs, id = as.factor(col)) %>% 
+    mutate(num_id = row_number() * 3)
+
+ggplot(replic_df) +
+    geom_point(aes(x = num_id, y = coefficient))
 
 
-x = test$draw
-x = c(1,2,3)
 
-1*all(diff(x)>0)
-
-%>% 
-    pivot_wider(id_cols = draw_num, 
-                names_from = educ, names_prefix = "D_", values_from = draw)
-draws %>% mutate()
-
-
-df_t <- tibble(id = 40:45, w = 10:15, x = 20:25, y = 30:35, z = 40:45)
-df_t
-df_t %>% rowwise() %>% mutate(test = 1*all(diff(w:z)>0))
-
-draws %>% mutate()
-
-head(draws)
-
-num_draws = 1000
-
-
-
-draws = mapply(draw, MoreArgs = list(mu = mu, sigma = sigma))
-
-
-
-d
-
-
-d =draw(mu = mu, sigma = sigma)
-
-plot(d)
-unique(df$educ)
-
-
-head(df)
 
