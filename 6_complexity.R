080ceed9d125b52806dc8aa01f98b8c1bf17bd20
TomBearpark <t.bearpark@googlemail.com>
2020-10-06 21:27:14 +0100

    draft version done

--- 6_complexity.R
+++ 6_complexity.R
@@ -1,13 +1,14 @@
 # Code for Exercise 6, ECO517 at Princeton
 
-# note: - make sure you have dplyr, ggplot2, car
+# note: - make sure you have all below
 #       packages up to date and installed to run this code
 #       - also, change the "dir" string to where you want to save outputs
 
 # Contents: 
 # 0. Set up environment and libraries 
-# 1 
-
+# 1. Frequentist F tests
+# 2. Bayesian BIC analysis
+# 3. CV glmnet
 
 
 ##################################
@@ -17,9 +18,10 @@ install.packages("glmnet")
 rm(list = ls())
 library(dplyr) # data manipulation, and piping
 library(ggplot2) # plots
-library(car)
+library(car) #Anova
 library(ggrepel) # Making text on scatter plots nicely spaced
-library(glmnet)
+library(glmnet) # CV ML routine
+
 set.seed(1) # Make random numbers replicable
 theme_set(theme_bw()) # ggplot theme
 
@@ -81,11 +83,8 @@ summary(lm.5)
 # 1.6 All the variables in items 4 and 5.
 lm.6 = lm(data = df, as.formula(paste0("logwage ~ educ + yob ", geq, dum)))
 
-
-
-# Extension... 
 # Can we visualise these? 
-# Compare model 2 to the model used in AK, checking out the treatmet effects
+# Compare model 2 to the model used in AK, checking out the coefficients 
 
 return_coefs = function(model){
     return(data.frame(var=names(coef(model)), coef=coef(model), row.names=NULL))
@@ -98,14 +97,12 @@ c2 = return_coefs(lm.2) %>%
 ggplot(data = c2) + 
     geom_point(aes(x = educ, y = marginal)) +
     geom_hline(yintercept = lm.0$coefficients["educ"], color = "red")
-
+ggsave(file = paste0(dir, "/coefficients_plot.png"))
 
 ####################################################################
 # 2 Frequentist tests
 
-
 # F-test - R\beta= \gamma
-
 # Model 0
 anova.0 = Anova(lm.0)
 
@@ -136,7 +133,8 @@ N = length(df$educ)
 
 # replicate 1 - make sure our function does the same thing as the cars package
 lmVR = lm(data = df, logwage ~ educ)
-all.equal(F_generate(lmVR, lm.1, N = N), anova.1$`F value`[2], tolerance = 0.01)
+all.equal(F_generate(lmVR, lm.1, N = N), anova.1$`F value`[2], 
+          tolerance = 0.01)
 
 # Calculate F stat on the dummies block for model 4
 F_generate(lm.4, lm.1, N)
@@ -144,6 +142,9 @@ F_generate(lm.4, lm.1, N)
 # Model 5
 F_generate(lm.5, lm.1, N)
 
+# Model 6
+F_generate(lm.6, lm.5, N)
+F_generate(lm.6, lm.4, N)
 
 
 ####################################################################
@@ -159,28 +160,15 @@ ggplot(data = bic_df) +
     theme(legend.position="none")+
     geom_text_repel(aes(x = df, y = BIC, label = model, color = model))
 
-
-
-
 ####################################################################
-# 3 Cross validation
+# 3 Cross validation - note, these take AGES to run, since its a pretty 
+# huge optimisation problem
 head(df)
 
 # Get outcome as a matrix - for use in cv.glmnet function
 y = df$logwage %>% as.matrix()
 
-# Get a dataframe of variables we want to use in CV
-dd = df  %>% 
-    select(educ, yob, starts_with("D_geq")) %>% 
-    mutate(educ_f = as.factor(educ), yob_f = as.factor(yob))
-
-# Turn it into a matrix of dummies
-X = model.matrix(~ educ + educ_f + educ:yob_f + educ:educ_f:yob_f, dd)
-
-# run k-fold cv
-fit = cv.glmnet(X, y)
-plot(fit)
-
+# Helped function for processing results
 get_results = function(fit){
     tmp_coeffs <- coef(fit)
     myResults <- data.frame(
@@ -189,28 +177,52 @@ get_results = function(fit){
     )
     return(myResults)
 }
-get_results(fit)
 
-
-# Try with more coefs...
+# Fefine coefs string...
 eq_dum = paste0(" + educ:D_geq_8 + educ:D_geq_12 + educ:D_geq_16 + educ:D_geq_20 ", 
                 " + D_geq_8 + D_geq_12 + D_geq_16 + D_geq_20 ")
 
-formula = paste("~ educ + educ_f + educ:yob_f + educ:educ_f:yob_f", eq_dum, 
+formula = paste("~ educ + educ_f + educ:yob_f + educ:educ_f:yob_f + educ_f:yob_f", eq_dum, 
                 collapse = " ") %>% 
     as.formula()
-
+# Create design matrix
 X_2 = model.matrix(formula, dd)
+
+# Run the CV
 fit2 = cv.glmnet(X_2, y)
-plot(fit2)
-get_results(fit2)
+fit2$lambda.min
+fit2$lambda.1se
+
+# Visualise them... 
+png(file=paste0(dir, "/cv_fit.png"))
+    plot(fit2)
+dev.off()
+    
+ # Print results
+g = get_results(fit2)
 
 
+# See how this optimal solution performs
+# Use these final results to see if we can do better on BIC than previously
+df = df %>% 
+    mutate(educ_f16 = 1*(educ == 16), 
+           educ.D_geq_8 = educ * D_geq_8, 
+           educ.D_geq_12 = educ * D_geq_12)
 
+lm.cv = lm(logwage ~ educ + educ_f16 + D_geq_16 + educ.D_geq_8 + educ.D_geq_12, df)
+summary(lm.cv)
 
+bic_df = BIC(lm.0, lm.1, lm.2, lm.3, lm.4, lm.5, lm.6, lm.cv)
 
+# Extract dataframe of results
+bic_df = data.frame(model=rownames(bic_df), bic_df, row.names=NULL)
 
+ggplot(data = bic_df) +
+    geom_point(aes(x = df, y = BIC, color = model)) +
+    theme(legend.position="none")+
+    geom_text_repel(aes(x = df, y = BIC, label = model, color = model))
 
+ggsave(paste0(dir, "/BIC_with_cv_results.png"))
 
 
 
