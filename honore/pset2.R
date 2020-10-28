a5508b3b3a9aa3367b0896b6b4b11c7c6c77a569
TomBearpark <t.bearpark@googlemail.com>
2020-10-28 16:15:00 +0000

    moving on to combining datasets across years

--- honore/pset2.R
+++ honore/pset2.R
@@ -6,6 +6,7 @@ library(dplyr)     # data manipulation
 library(patchwork) # combining ggplot objects
 library(stargazer) # making nice regression output tables
 library(fread)     # fread function for easy data reading 
+library(cars)      # useful f-stat functions
 theme_set(theme_bw())
 
 # Directory string for outputs / inputs
@@ -44,7 +45,7 @@ plot_correlations = function(df){
   ggplot(data = df) +
     geom_point(aes(x = ED, y = WAGE)) + 
     ggtitle(paste0("Corr: ", corr)) +
-  ggplot(data = df) +
+    ggplot(data = df) +
     geom_point(aes(x = ED, y = LNWAGE))+ 
     ggtitle(paste0("Corr: ", corr_ln))
 }
@@ -59,13 +60,12 @@ ggsave(paste0(dir, "/correlations_78.png"), p1)
 
 # 2- regression model
 lm78 = lm(data = df78, 
-   LNWAGE ~ FE + UNION + HISP + NONWH + ED + EX + EXSQ)
-stargazer(lm78)
+          LNWAGE ~ FE + UNION + HISP + NONWH + ED + EX + EXSQ)
 
 # 4 - at what level of experience is wage maximised
 coefsm = coef(lm78)
 exper = function(x) coefsm["(Intercept)"] + 
-    coefsm["EX"]* x + coefsm["EXSQ"]* x ^2
+  coefsm["EX"]* x + coefsm["EXSQ"]* x ^2
 
 maxLNwage = as.numeric( -0.5 * (coefsm["EX"] / coefsm["EXSQ"]))
 
@@ -86,9 +86,9 @@ confint(lm78, 'FE', level=0.95)
 # 7. Fail to reject since its in the interval
 confint(lm78, 'HISP', level=0.9)
 
-# 8. 
+# 8. Run an expanded regression
 lm78_ex = lm(data = df78, 
-          LNWAGE ~ FE + UNION + HISP + NONWH + ED + EX + EXSQ + EX3 + EX4 + EX5)
+             LNWAGE ~ FE + UNION + HISP + NONWH + ED + EX + EXSQ + EX3 + EX4 + EX5)
 
 stargazer(lm78, lm78_ex)
 
@@ -102,28 +102,42 @@ lm78_noEX = lm(data = df78,
 # Calculate F-statistic
 anova(lm78_ex,lm78_noEX)  
 
+
+
+
+
+#####
 # 11. Re-run everything for 85 data
+
+# load in data
 df85= get_data(dir = dir, name = "Cps85")
-# Consider the correlations
+# 1 Consider the correlations
 plot_correlations(df85)
-# Run regression
+# 2 Run regressions
 lm85 = lm(data = df85, 
           LNWAGE ~ FE + UNION + HISP + NONWH + ED + EX + EXSQ)
-coef85 = coef(lm85)
-# max ln wage by experience - 32
-maxLNwage = as.numeric( -0.5 * (coef85["EX"] / coef85["EXSQ"]))
-confint(lm85, 'FE', level=0.95)
-
-# 7. Fail to reject since its in the interval
-confint(lm85, 'HISP', level=0.9)
-
-# 8. 
-lm85_ex = lm(data = df78, 
+lm85_ex = lm(data = df85, 
              LNWAGE ~ FE + UNION + HISP + NONWH + 
                ED + EX + EXSQ + EX3 + EX4 + EX5)
-summary(lm85_ex)
-# linearHypothesis(lm85_ex, c("EX3=0", "EX4=0", "EX5=0"))
+stargazer(lm85, lm85_ex)
+# 3 - interpretations 
+# 4 Max value of wage by experience 
+coef85 = coef(lm85)
+maxLNwage = as.numeric( -0.5 * (coef85["EX"] / coef85["EXSQ"]))
+# 5 Calculate difference in exected wage for 16 vs 12 years schooling
+coef85["ED"] * (12 - 16) + coef85["EX"] *(12 - 8)  + coef85["EXSQ"] * (12^2 - 8^2)
+# 6 confidence interval for alpha_F
+confint(lm85, 'FE', level=0.95)
+# 7. Fail to reject since its in the interval - CI for alpha_H
+confint(lm85, 'HISP', level=0.9)
+# 8 - regressoin ran in part 2
+# 9 can see this in outputs of regression table in part 2
+# 10. Run a more restricted regression, calculate F-stat using anova
+lm85_noEX = lm(data = df85, 
+             LNWAGE ~ FE + UNION + HISP + NONWH + ED)
+anova(lm85_ex,lm85_noEX)  
 
+#####
 
 # 12
 df_long = bind_rows(df78 %>% mutate(year = 0), 
@@ -133,9 +147,10 @@ lm_joint = lm(data = df_long,
               LNWAGE ~ 0+ FE + UNION + HISP + NONWH + ED + EX + EXSQ + 
                 year * (FE + UNION + HISP + NONWH + ED + EX + EXSQ ))
 
-# linearHypothesis(lm_joint, c("FE:year=0", "UNION:year=0",   "HISP:year=0",  
-                            "NONWH:year=0",  "ED:year=0",     "EX:year=0",
-                            "EXSQ:year=0" ))
+linearHypothesis(lm_joint, c("FE:year=0", "UNION:year=0",   "HISP:year=0",
+  "NONWH:year=0",  "ED:year=0",     "EX:year=0",
+  "EXSQ:year=0" ))
+
 # 13
 df_long =  df_long %>% 
   mutate(WAGE_inf = ifelse(year == 1, WAGE / 1.649, WAGE)) %>% 
@@ -162,14 +177,3 @@ stargazer(lm78, lm78_ex, lm85, lm85_ex)
 x = rnorm(800)
 y = x^2
 summary(lm(y ~ x))
-
-
-
-
-
-
-
-
-
-
-
