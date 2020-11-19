308292aa6ee65988c3124bf4bb0d1064129a8922
TomBearpark <t.bearpark@googlemail.com>
2020-11-19 16:38:23 +0000

    data set generaiton working

--- honore/pset5.R
+++ honore/pset5.R
@@ -4,9 +4,18 @@ rm(list = ls())
 library(tidyverse)
 library(ggplot2)
 
+# Data set properties 
 M = 3
 n = 1000
-bm = c(0,0,0)
+
+# Set parameters
+d0 = c(1,1,1)
+d1 = c(1,1,1)
+a1 = c(1,1,1)
+a2 = c(1,1,1)
+b = c(0,0,0)
+a3 = c(0,0,0)
+
 
 # Generate dataset
 
@@ -22,19 +31,31 @@ df = data.frame(
 # Variables that are specific to each equation, m
 for (m in 1:M) {
   for (i in 1:3) df[paste0("v",i,"_m",m)] = rnorm(n) 
-  for (i in 0:1) df[paste0("d",i,"_m",m)] = rep(1, n)
-  for (i in 1:2) df[paste0("a",i,"_m",m)] = rep(1, n)
-  df[paste0("x3_pr_beta", m)] = df[paste0("x3_", m)] * bm[m]
   df[paste0("w2_m", m)] = rnorm(n)
-  df[paste0("x2_m", m)] = df[paste0("x3_", m)] * bm[m]
   df[paste0("x1_m", m)] = rnorm(n)
+  df[paste0("x2_m", m)] = df[paste0("w1")] + df[paste0("w2_m", m)]
 }
 # Specify alpha vectors
-a1 = c(1,1,1)
-a2 = c(1,1,1)
+
 # Generate the dependent variables
 for (m in 1:M) {
-  df[paste0("z_m", m)] = 
-  df[paste0("y_m", m)] = df[paste0("d0_m", m)] + 
-        
+  # Generate z
+  df[paste0("z_m", m)] = df[paste0("x1_m", m)] * a1[m] + 
+    df[paste0("x2_m", m)] * a2[m] + 
+    (df["x3_1"] + df["x3_1"] + df["x3_1"]) * a3[m] + 
+    df[paste0("v1_m",m)] + df[paste0("v3_m",m)] + df["u2"]
+  
+  # Generate y
+  df[paste0("y_m", m)] = d0[m] + df[paste0("z_m", m)] * d1[m] + 
+    (df["x3_1"] + df["x3_1"] + df["x3_1"]) * b[m] + 
+    df[paste0("v1_m",m)] + df[paste0("v2_m",m)] + df["u1"]
 }
+
+head(df)
+
+
+
+
+
+
+
